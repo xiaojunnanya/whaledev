99079a01f20b01c0b8a1cd9350a9e9fe941efa64
xiaojunnanya <xjntofly@sina.com>
2024-10-24 15:36:37 +0800

    feat: 添加icon

--- packages/frontEnd/vite.config.ts
+++ packages/frontEnd/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: 'localhost',
+    port: 5173,
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: path => path.replace('/api', ''),
+      },
+    },
+  },
 })
