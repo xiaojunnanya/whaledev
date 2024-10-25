6573995d271d8fc8eeb377fa1a933acb05a4e69a
xiaojunnanya <xjntofly@sina.com>
2024-10-26 01:46:38 +0800

    feat: 添加端口提示

--- packages/backEnd/src/main.ts
+++ packages/backEnd/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3001);
+  console.log('开始使用nest，端口号3001');
 }
 bootstrap();
