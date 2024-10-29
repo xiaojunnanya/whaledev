1b74bfc0bcf1fb325b31bac7e4685dc14d485a9f
xiaojunnanya <xjntofly@sina.com>
2024-10-29 17:42:44 +0800

    feat: 添加redis重连次数

--- packages/backEnd/src/global/redis.module.ts
+++ packages/backEnd/src/global/redis.module.ts
@@ -10,6 +10,13 @@ import Redis from 'ioredis'
         return new Redis({
           host: 'localhost',
           port: 6379,
+          retryStrategy(times) {
+            const maxRetries = 5 // 最大重连次数
+            if (times >= maxRetries) {
+              return null // 达到最大重连次数后停止重连
+            }
+            return Math.min(times * 1000, 3000)
+          },
         })
       },
     },
