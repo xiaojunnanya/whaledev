f1311adc9f5ea6d975d9d3341966278656670fae
xiaojunnanya <xjntofly@sina.com>
2024-11-22 10:55:29 +0800

    feat: 实现文件增删改

---
+++ packages/frontEnd/src/views/ReactPlay/CodeEditor/FileNameList/FileNameItem/index.tsx
@@ -0,0 +1,87 @@
+import { memo, MouseEventHandler, useEffect, useRef, useState } from 'react'
+import { FileNameItemStyled } from './style'
+import { Popconfirm } from 'antd'
+
+export interface IProps {
+  value: string
+  actived: boolean
+  creating: boolean
+  onEditComplete: (name: string) => void
+  onClick: () => void
+  onRemove: () => void
+  readonly: Boolean
+}
+
+export default memo((props: IProps) => {
+  const {
+    value,
+    actived = false,
+    onClick,
+    onEditComplete,
+    creating,
+    onRemove,
+    readonly,
+  } = props
+  const [name, setName] = useState(value)
+  const [editing, setEditing] = useState(creating)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (creating) {
+      inputRef?.current?.focus()
+    }
+  }, [creating])
+
+  const handleDoubleClick = () => {
+    setEditing(true)
+    setTimeout(() => {
+      inputRef?.current?.focus()
+    }, 0)
+  }
+
+  const hanldeInputBlur = () => {
+    setEditing(false)
+    onEditComplete(name)
+  }
+
+  return (
+    <FileNameItemStyled
+      className={`tab-item ${actived ? 'actived' : ''}`}
+      onClick={onClick}
+    >
+      {editing ? (
+        <input
+          ref={inputRef}
+          className="tabs-item-input"
+          value={name}
+          onBlur={hanldeInputBlur}
+          onChange={e => setName(e.target.value)}
+        />
+      ) : (
+        <>
+          <span onDoubleClick={!readonly ? handleDoubleClick : () => {}}>
+            {name}
+          </span>
+          {!readonly ? (
+            <Popconfirm
+              title="确认删除该文件吗？"
+              okText="确定"
+              cancelText="取消"
+              onConfirm={e => {
+                e?.stopPropagation()
+                onRemove()
+              }}
+            >
+              <span style={{ marginLeft: 5, display: 'flex' }}>
+                <svg width="12" height="12" viewBox="0 0 24 24">
+                  <line stroke="#999" x1="18" y1="6" x2="6" y2="18"></line>
+                  <line stroke="#999" x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </span>
+            </Popconfirm>
+          ) : null}
+        </>
+      )}
+    </FileNameItemStyled>
+  )
+})
--- packages/frontEnd/src/views/ReactPlay/CodeEditor/FileNameList/index.tsx
+++ packages/frontEnd/src/views/ReactPlay/CodeEditor/FileNameList/index.tsx
@@ -1,27 +1,68 @@
-import { useReactPlay } from '@/stores/reactplay'
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+  useReactPlay,
+} from '@/stores/reactplay'
 import { memo, useEffect, useState } from 'react'
 import { FileNameListStyled } from './style'
+import FileNameItem from './FileNameItem'
 
 export default memo(() => {
-  const { files, selectedFileName, setSelectedFileName } = useReactPlay()
-
+  const {
+    files,
+    selectedFileName,
+    setSelectedFileName,
+    updateFileName,
+    addFile,
+    removeFile,
+  } = useReactPlay()
+  const [creating, setCreating] = useState(false)
   const [tabs, setTabs] = useState([''])
 
   useEffect(() => {
     setTabs(Object.keys(files))
   }, [files])
 
+  const handleEditComplete = (name: string, prevName: string) => {
+    updateFileName(prevName, name)
+    setSelectedFileName(name)
+    setCreating(false)
+  }
+
+  const addTab = () => {
+    addFile('Com' + Math.random().toString().slice(2, 6) + '.tsx')
+    setCreating(true)
+  }
+
+  const handleRemove = (name: string) => {
+    removeFile(name)
+    setSelectedFileName(ENTRY_FILE_NAME)
+  }
+  const readonlyFileNames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ]
   return (
     <FileNameListStyled>
       {tabs.map((item, index) => (
-        <div
-          key={index}
+        <FileNameItem
+          key={index + item}
+          value={item}
+          actived={selectedFileName === item}
           onClick={() => setSelectedFileName(item)}
-          className={`tab-item ${selectedFileName === item ? 'actived' : ''}`}
-        >
-          {item}
-        </div>
-      ))}
+          onEditComplete={(name: string) => handleEditComplete(name, item)}
+          creating={creating && index === tabs.length - 1}
+          onRemove={() => {
+            handleRemove(item)
+          }}
+          readonly={readonlyFileNames.includes(item)}
+        />
+      ))}{' '}
+      <div className="add" onClick={addTab}>
+        +
+      </div>
     </FileNameListStyled>
   )
 })
