2e4002bc7de238421f8b0aefaa21a6f699e286d4
xiaojunnanya <2376974436@qq.com>
2025-01-09 21:49:41 +0800

    chore: 为gitignore添加注释

--- packages/backEnd/src/config/index-gitignore.ts
+++ packages/backEnd/src/config/index-gitignore.ts
@@ -1,9 +1,12 @@
-export const EMAIL_USER = 'xxx'
-export const EMAIL_PASS = 'xxx'
+// 配置发送邮箱验证码
+export const EMAIL_USER = 'xxx' // 邮箱
+export const EMAIL_PASS = 'xxx' // 邮箱密钥
 
+
+// 用户信息
 export const AUTHOR = {
-  NAME: 'xxx',
-  PROJECTNAME: 'xxx',
-  secret: 'xxx',
-  expiresIn: 'xxx',
+  NAME: 'xxx', // 用户名
+  PROJECTNAME: 'xxx', // 项目名称
+  secret: 'xxx', // token密钥
+  expiresIn: 'xxx', // token时间
 }
