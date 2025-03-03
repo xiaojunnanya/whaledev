7e34f467b808583720f54f2cf5dfca6ff56b4913
xiaojunnanya <xjntofly@sina.com>
2025-03-03 16:48:12 +0800

    feat: 添加白屏监测

---
+++ packages/frontEnd/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,61 @@
+import { Button, Result } from 'antd'
+import { Component, ErrorInfo, ReactNode } from 'react'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    console.error(error, 'error')
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error(error, errorInfo)
+    // 可以进行错误上报
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <>
+          <Result
+            status="500"
+            title="页面异常"
+            subTitle="您可以尝试以下操作"
+            extra={
+              <>
+                <Button type="primary" onClick={() => window.location.reload()}>
+                  刷新页面
+                </Button>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      'https://github.com/xiaojunnanya/whaledev/issues',
+                    )
+                  }
+                >
+                  联系我们
+                </Button>
+              </>
+            }
+          />
+        </>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
--- packages/frontEnd/src/main.tsx
+++ packages/frontEnd/src/main.tsx
@@ -7,17 +7,20 @@ import { ThemeProvider } from 'styled-components'
 import theme from './assets/theme'
 import './assets/css/index.css'
 import AuthRouter from './components/AuthRouter/index.tsx'
+import ErrorBoundary from './components/ErrorBoundary/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <AuthRouter>
-      <ThemeProvider theme={theme}>
-        <Suspense fallback={<Loading isBigLoading={true} />}>
-          <App />
-        </Suspense>
-      </ThemeProvider>
-    </AuthRouter>
-  </BrowserRouter>,
+  <ErrorBoundary>
+    <BrowserRouter>
+      <AuthRouter>
+        <ThemeProvider theme={theme}>
+          <Suspense fallback={<Loading isBigLoading={true} />}>
+            <App />
+          </Suspense>
+        </ThemeProvider>
+      </AuthRouter>
+    </BrowserRouter>
+  </ErrorBoundary>,
 )
 
 console.info(`
