762d679e81ad69cebb9a55aedcda7ae7ff846c68
xiaojunnanya <xjntofly@sina.com>
2025-04-03 18:02:15 +0800

    feat: 添加左侧配置ai

--- packages/frontEnd/src/views/AiView/index.tsx
+++ packages/frontEnd/src/views/AiView/index.tsx
@@ -1,6 +1,34 @@
 import AiContent from '@/components/Ai/AiContent'
+import { Conversations, ConversationsProps } from '@ant-design/x'
+import { GetProp, theme } from 'antd'
 import { memo } from 'react'
+import { AiViewStyled } from './style'
+
+const items: GetProp<ConversationsProps, 'items'> = Array.from({
+  length: 4,
+}).map((_, index) => ({
+  key: `item${index + 1}`,
+  label: `Conversation Item ${index + 1}`,
+  disabled: index === 3,
+  group: index === 3 ? 'Group2' : 'Group1',
+}))
 
 export default memo(() => {
-  return <AiContent></AiContent>
+  const { token } = theme.useToken()
+
+  return (
+    <AiViewStyled>
+      <Conversations
+        items={items}
+        defaultActiveKey="item1"
+        style={{
+          width: 256,
+          background: token.colorBgContainer,
+          borderRadius: token.borderRadius,
+        }}
+        groupable
+      />
+      <AiContent></AiContent>
+    </AiViewStyled>
+  )
 })
