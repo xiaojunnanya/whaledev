1969fe7259ef46e2750e485d8a9c28a9d3e680c4
xiaojunnanya <xjntofly@sina.com>
2025-04-18 13:56:01 +0800

    feat: 修改nest配置

--- packages/backEnd/src/main.ts
+++ packages/backEnd/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   })
 
   // 静态资源的展示
-  app.useStaticAssets('src/assets/images', { prefix: '/img' })
+  app.useStaticAssets('./src/assets/images', { prefix: '/img' })
 
   await app.listen(3173)
   Logger.log('开始使用nest，端口号3173')
