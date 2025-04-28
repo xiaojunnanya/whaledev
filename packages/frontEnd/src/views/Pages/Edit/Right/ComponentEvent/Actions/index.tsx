bcd6cda107537cfc2ecbba4a07064f20e5d3940e
xiaojunnanya <2376974436@qq.com>
2025-04-28 23:23:05 +0800

    feat: 未实现的编排

--- packages/frontEnd/src/views/Pages/Edit/Right/ComponentEvent/Actions/index.tsx
+++ packages/frontEnd/src/views/Pages/Edit/Right/ComponentEvent/Actions/index.tsx
@@ -40,139 +40,139 @@ export const itemsActions: itemsType[] = [
       },
     ],
   },
-  {
-    label: '表单',
-    key: 'form',
-    children: [
-      {
-        label: '表单功能',
-        key: 'formUse',
-        render: () => {
-          return <div>表单重置/提交/验证/赋值/获取表单值</div>
-        },
-      },
-    ],
-  },
-  {
-    label: '弹框',
-    key: 'modal',
-    children: [
-      {
-        label: '弹框功能',
-        key: 'modalUse',
-        render: () => {
-          return <div>打开弹框/关闭弹框</div>
-        },
-      },
-      {
-        label: '抽屉功能',
-        key: 'drawerUse',
-        render: () => {
-          return <div>打开抽屉/关闭抽屉</div>
-        },
-      },
-      {
-        label: '确认框',
-        key: 'confirm',
-        render: () => {
-          return <div>确认框</div>
-        },
-      },
-      {
-        label: '全局提示',
-        key: 'globalTip',
-        render: () => {
-          return <div>全局提示</div>
-        },
-      },
-      {
-        label: '消息通知',
-        key: 'messageNotify',
-        render: () => {
-          return <div>消息通知</div>
-        },
-      },
-    ],
-  },
-  {
-    label: '请求',
-    key: 'request',
-    children: [
-      {
-        label: '发送请求',
-        key: 'sendRequest',
-        render: () => {
-          return <div>发送请求</div>
-        },
-      },
-      {
-        label: '文件下载',
-        key: 'downloadFile',
-        render: () => {
-          return <div>文件下载</div>
-        },
-      },
-    ],
-  },
-  {
-    label: '组件',
-    key: 'component',
-    children: [
-      {
-        label: '组件显隐',
-        key: 'componentVisible',
-        render: () => {
-          return <div>组件显隐</div>
-        },
-      },
-      {
-        label: '组件禁用',
-        key: 'componentDisabled',
-        render: () => {
-          return <div>组件禁用</div>
-        },
-      },
-      {
-        label: '组件方法',
-        key: 'componentMethod',
-        render: () => {
-          return <div>组件方法</div>
-        },
-      },
-      {
-        label: '变量赋值',
-        key: 'variableSet',
-        render: () => {
-          return <div>变量赋值</div>
-        },
-      },
-      {
-        label: '刷新组件',
-        key: 'refreshComponent',
-        render: () => {
-          return <div>刷新组件</div>
-        },
-      },
-    ],
-  },
+  // {
+  //   label: '表单',
+  //   key: 'form',
+  //   children: [
+  //     {
+  //       label: '表单功能',
+  //       key: 'formUse',
+  //       render: () => {
+  //         return <div>表单重置/提交/验证/赋值/获取表单值</div>
+  //       },
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: '弹框',
+  //   key: 'modal',
+  //   children: [
+  //     {
+  //       label: '弹框功能',
+  //       key: 'modalUse',
+  //       render: () => {
+  //         return <div>打开弹框/关闭弹框</div>
+  //       },
+  //     },
+  //     {
+  //       label: '抽屉功能',
+  //       key: 'drawerUse',
+  //       render: () => {
+  //         return <div>打开抽屉/关闭抽屉</div>
+  //       },
+  //     },
+  //     {
+  //       label: '确认框',
+  //       key: 'confirm',
+  //       render: () => {
+  //         return <div>确认框</div>
+  //       },
+  //     },
+  //     {
+  //       label: '全局提示',
+  //       key: 'globalTip',
+  //       render: () => {
+  //         return <div>全局提示</div>
+  //       },
+  //     },
+  //     {
+  //       label: '消息通知',
+  //       key: 'messageNotify',
+  //       render: () => {
+  //         return <div>消息通知</div>
+  //       },
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: '请求',
+  //   key: 'request',
+  //   children: [
+  //     {
+  //       label: '发送请求',
+  //       key: 'sendRequest',
+  //       render: () => {
+  //         return <div>发送请求</div>
+  //       },
+  //     },
+  //     {
+  //       label: '文件下载',
+  //       key: 'downloadFile',
+  //       render: () => {
+  //         return <div>文件下载</div>
+  //       },
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: '组件',
+  //   key: 'component',
+  //   children: [
+  //     {
+  //       label: '组件显隐',
+  //       key: 'componentVisible',
+  //       render: () => {
+  //         return <div>组件显隐</div>
+  //       },
+  //     },
+  //     {
+  //       label: '组件禁用',
+  //       key: 'componentDisabled',
+  //       render: () => {
+  //         return <div>组件禁用</div>
+  //       },
+  //     },
+  //     {
+  //       label: '组件方法',
+  //       key: 'componentMethod',
+  //       render: () => {
+  //         return <div>组件方法</div>
+  //       },
+  //     },
+  //     {
+  //       label: '变量赋值',
+  //       key: 'variableSet',
+  //       render: () => {
+  //         return <div>变量赋值</div>
+  //       },
+  //     },
+  //     {
+  //       label: '刷新组件',
+  //       key: 'refreshComponent',
+  //       render: () => {
+  //         return <div>刷新组件</div>
+  //       },
+  //     },
+  //   ],
+  // },
   {
     label: '其他',
     key: 'other',
     children: [
-      {
-        label: '复制内容',
-        key: 'copyContent',
-        render: () => {
-          return <div>复制内容</div>
-        },
-      },
-      {
-        label: '定时器',
-        key: 'timer',
-        render: () => {
-          return <div>定时器</div>
-        },
-      },
+      // {
+      //   label: '复制内容',
+      //   key: 'copyContent',
+      //   render: () => {
+      //     return <div>复制内容</div>
+      //   },
+      // },
+      // {
+      //   label: '定时器',
+      //   key: 'timer',
+      //   render: () => {
+      //     return <div>定时器</div>
+      //   },
+      // },
       {
         label: '脚本运行',
         key: 'scriptRun',
