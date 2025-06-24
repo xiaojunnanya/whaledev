b96c1b89c445a972eea6ba50ab79cfda8d48d1b7
xiaojunnanya <2376974436@qq.com>
2025-06-24 21:37:36 +0800

    build: 添加分包[D[D[D[D[D[D[Dvite

--- packages/frontEnd/vite.config.ts
+++ packages/frontEnd/vite.config.ts
@@ -53,5 +53,92 @@ export default defineConfig({
   build: {
     outDir: '../../build/front',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        // 入口文件命名规则
+        entryFileNames: 'js/[name].[hash].js',
+        // 动态 import 的 chunk 命名规则
+        chunkFileNames: 'js/[name].[hash].js',
+        // 静态资源分类打包
+        assetFileNames: assetInfo => {
+          const ext = assetInfo.name?.split('.').pop()?.toLowerCase()
+          if (!ext) return 'assets/[name].[hash][extname]'
+
+          if (['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'].includes(ext)) {
+            return 'images/[name].[hash][extname]'
+          }
+
+          if (['css'].includes(ext)) {
+            return 'css/[name].[hash][extname]'
+          }
+
+          if (['woff', 'woff2', 'eot', 'ttf', 'otf'].includes(ext)) {
+            return 'fonts/[name].[hash][extname]'
+          }
+
+          return 'assets/[name].[hash][extname]'
+        },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const packageName = id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+
+            // // React
+            // if (
+            //   [
+            //     'react',
+            //     'react-dom',
+            //     'react-router-dom',
+            //     'zustand',
+            //     'typescript',
+            //     'styled-components',
+            //   ].includes(packageName)
+            // ) {
+            //   return 'react-vendor'
+            // }
+
+            // // Antd
+            // if (
+            //   [
+            //     'antd',
+            //     '@ant-design/icons',
+            //     '@ant-design/x',
+            //     'antd-img-crop',
+            //   ].includes(packageName)
+            // ) {
+            //   return 'antd-vendor'
+            // }
+
+            // // 编辑器
+            // if (
+            //   [
+            //     '@monaco-editor/react',
+            //     '@babel/standalone',
+            //     '@typescript/ata',
+            //   ].includes(packageName)
+            // ) {
+            //   return 'editor-vendor'
+            // }
+
+            // 工具
+            if (
+              [
+                'lodash-es',
+                'markdown-it',
+                'file-saver',
+                'copy-to-clipboard',
+                'spark-md5',
+                'jszip',
+                'uuid',
+              ].includes(packageName)
+            ) {
+              return 'misc-vendor'
+            }
+          }
+        },
+      },
+    },
   },
 })
